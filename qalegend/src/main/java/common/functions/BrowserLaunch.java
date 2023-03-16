fe9ce5174ffe1c2e830ce760455b38c433f21bb6
arath <arath@LAPTOP-GKHRCD72>
2023-03-16 21:55:42 +0530

    groups and parameter

--- qalegend/src/main/java/common/functions/BrowserLaunch.java
+++ qalegend/src/main/java/common/functions/BrowserLaunch.java
@@ -3,19 +3,40 @@ package common.functions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class BrowserLaunch {
 	public static WebDriver driver;
 
+	@Parameters("browserName")
 	@BeforeMethod(alwaysRun = true)
-	public void urlLaunch() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		System.setProperty("webdriver.chrome.driver", "C://SeleniumDrivers//chromedriver.exe");
-		driver = new ChromeDriver(options);
+	public void urlLaunch(String browserName) {
+		switch (browserName) {
+		case "chrome":
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			System.setProperty("webdriver.chrome.driver", "C://SeleniumDrivers//chromedriver.exe");
+			driver = new ChromeDriver(options);
+			break;
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "C://SeleniumDrivers//geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			System.setProperty("webdriver.edge.driver", "C://SeleniumDrivers//msedgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+		default:
+			System.out.println("Invalid browser");
+			break;
+		}
+
 		driver.get("https://qalegend.com/billing/public/login");
 		driver.manage().window().maximize();
 

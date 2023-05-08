36f39dd2644f54d055744d24bdf3d984c208d754
arath <arath@LAPTOP-GKHRCD72>
2023-05-08 19:43:23 +0530

    listen

--- qalegend/src/main/java/qalegend/utils/Listener.java
+++ qalegend/src/main/java/qalegend/utils/Listener.java
@@ -1,32 +1,38 @@
 package qalegend.utils;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.annotations.Parameters;
 
 import common.functions.BrowserLaunch;
+import constants.Constant;
+
 
 public class Listener extends BrowserLaunch implements ITestListener {
-//	Screenshot screenshotCapture=new Screenshot();
+	
+		// TODO Auto-generated method stub
+	
 	public void onTestStart(ITestResult result) {
 		
+//		System.out.println("start "+driver);
+		System.out.println("x="+x);
 	}
 	
 	public void onTestSuccess(ITestResult result) {
-		
+		System.out.println("success "+driver);
+
 	}
 	
 	public void onTestFailure(ITestResult result) {
 		
-		try {
-			
-			Screenshot.takeScreenshot(driver);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 	}
 	
